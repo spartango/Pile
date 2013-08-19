94e628164938c43cf12a9cdce01d393d4cfb7242
Anand Gupta <spartango@gmail.com>
2013-08-19 02:02:35 -0400

    Added a stub for pubmed

--- src/main/java/us/percept/pile/repo/PaperSource.java
+++ src/main/java/us/percept/pile/repo/PaperSource.java
@@ -1,19 +1,11 @@
 package us.percept.pile.repo;
 
-import us.percept.pile.model.Paper;
-
-import java.util.Collection;
-
 /**
  * User: spartango
  * Date: 8/2/13
  * Time: 6:28 PM
  */
 public interface PaperSource {
-    public Paper getPaper(String identifier);
-
-    public Collection<Paper> findPapers(String query);
-
     // Asynchronous requests
     public void requestPaper(String identifier);
 
---
+++ src/main/java/us/percept/pile/repo/pubmed/PubmedSource.java
@@ -0,0 +1,18 @@
+package us.percept.pile.repo.pubmed;
+
+import us.percept.pile.repo.AsyncPaperSource;
+
+/**
+ * Author: spartango
+ * Date: 8/19/13
+ * Time: 2:02 AM.
+ */
+public class PubmedSource extends AsyncPaperSource {
+    @Override public void requestPaper(String identifier) {
+        //TODO implement PubmedSource.requestPaper
+    }
+
+    @Override public void requestSearch(String query) {
+        //TODO implement PubmedSource.requestSearch
+    }
+}
